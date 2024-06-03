4a8bb1854e922452290242b8b49d29bde1706da2
Anthony Radose <asradose@gmail.com>
2024-06-03 13:46:51 +0100

    Add product size logic

--- products/models.py
+++ products/models.py
@@ -33,7 +33,8 @@ class Product(models.Model):
     photographer = models.CharField(max_length=254)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    frame_size = models.CharField(max_length=2, choices=FRAME_SIZES, default='m')
+    # frame_size = models.CharField(max_length=2, choices=FRAME_SIZES, default='m')
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
         return self.name
