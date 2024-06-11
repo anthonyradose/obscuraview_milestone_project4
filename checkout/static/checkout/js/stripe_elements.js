10c48b3552ec767c1bcb23451a5f79e9bfc32439
Anthony Radose <asradose@gmail.com>
2024-06-11 18:19:40 +0100

    Add loading overlay

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,75 +1,71 @@
-const stripe_public_key = document
-  .getElementById("id_stripe_public_key")
-  .textContent.slice(1, -1);
-const client_secret = document
-  .getElementById("id_client_secret")
-  .textContent.slice(1, -1);
-  console.log("Stripe Public Key:", stripe_public_key);
-  console.log("Client Secret:", client_secret);
-const stripe = Stripe(stripe_public_key);
+const stripePublicKey = document.getElementById('id_stripe_public_key').textContent.slice(1, -1);
+const clientSecret = document.getElementById('id_client_secret').textContent.slice(1, -1);
+const stripe = Stripe(stripePublicKey);
 const elements = stripe.elements();
 const style = {
-  base: {
-    color: "#000",
-    fontFamily: '"Roboto", sans-serif',
-    fontSmoothing: "antialiased",
-    fontSize: "16px",
-    "::placeholder": {
-      color: "#aab7c4",
+    base: {
+        color: '#000',
+        fontFamily: '"Roboto", sans-serif',
+        fontSmoothing: 'antialiased',
+        fontSize: '16px',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
     },
-  },
-  invalid: {
-    color: "#dc3545",
-    iconColor: "#dc3545",
-  },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
 };
-const card = elements.create("card", { style: style });
-card.mount("#card-element");
+const card = elements.create('card', {style: style});
+card.mount('#card-element');
 
-const error_div = document.getElementById("card-errors");
 // Handle realtime validation errors on the card element
-card.addEventListener("change", function (event) {
-  if (event.error) {
-    let html = `
+card.addEventListener('change', function (event) {
+    const errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        let html = `
             <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
+                <i class="bi bi-x"></i>
             </span>
             <span>${event.error.message}</span>
         `;
-    error_div.innerHTML = html;
-  } else {
-    error_div.textContent = "";
-  }
+        errorDiv.innerHTML = html;
+    } else {
+        errorDiv.textContent = '';
+    }
 });
 
-const form = document.getElementById("payment-form");
-const submit_button = document.getElementById("submit-button");
+// Handle form submit
+const form = document.getElementById('payment-form');
 
-form.addEventListener("submit", function (ev) {
-  ev.preventDefault();
-  card.update({ disabled: true });
-  submit_button.disabled = true;
-
-  stripe
-    .confirmCardPayment(client_secret, {
-      payment_method: {
-        card: card,
-      },
-    })
-    .then(function (result) {
-      if (result.error) {
-        let html = `
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    document.getElementById('submit-button').setAttribute('disabled', true);
+    document.getElementById('payment-form').style.display = 'none';
+    document.getElementById('loading-overlay').style.display = 'block';
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            const errorDiv = document.getElementById('card-errors');
+            let html = `
                 <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
+                <i class="bi bi-x"></i>
                 </span>
                 <span>${result.error.message}</span>`;
-        error_div.innerHTML = html;
-        card.update({ disabled: false });
-        submit_button.disabled = false;
-      } else {
-        if (result.paymentIntent.status === "succeeded") {
-          form.submit();
+            errorDiv.innerHTML = html;
+            document.getElementById('payment-form').style.display = 'block';
+            document.getElementById('loading-overlay').style.display = 'none';
+            card.update({ 'disabled': false});
+            document.getElementById('submit-button').removeAttribute('disabled');
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
         }
-      }
     });
 });
