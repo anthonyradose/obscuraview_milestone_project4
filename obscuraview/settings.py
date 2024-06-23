5119254fb4cd2baa1153ca0a58558bbe4840f38f
Anthony Radose <asradose@gmail.com>
2024-06-23 18:21:38 +0100

    Add Heroku host to ALLOWED_HOSTS

--- obscuraview/settings.py
+++ obscuraview/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['obscuraview-milestone-project4.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['obscuraview-milestone-project4-72812c987ff0.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
