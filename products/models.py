12c62dadd8ad791021adc630efb2cc631a30f300
Anthony Radose <asradose@gmail.com>
2024-11-06 18:19:03 +0000

    Make product image required to fix form validation

--- products/models.py
+++ products/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     photographer = models.CharField(max_length=254)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField()
     has_sizes = models.BooleanField(default=True, null=True, blank=True)
 
     def __str__(self):
